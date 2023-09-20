603c546bfe4d2886659a00814f0700cd9730f88e
Anirban Kar <anirban.k59@gmail.com>
2023-09-20 17:56:01 +0530

    add new program staircase

---
+++ staircase.py
@@ -0,0 +1,17 @@
+def build_stair_case(n: int):
+    steps = 0
+    req_blocks = 0
+
+    while req_blocks < n:
+        steps += 1
+        req_blocks += 1
+        n -= req_blocks
+    return steps
+
+assert build_stair_case(0) == 0
+assert build_stair_case(1) == 1
+assert build_stair_case(2) == 1
+assert build_stair_case(3) == 2
+assert build_stair_case(5) == 2
+assert build_stair_case(6) == 3
+assert build_stair_case(9) == 3
