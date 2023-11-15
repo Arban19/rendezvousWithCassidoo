fcfb9430ef18a303573ed77187eaa70b26dd71fc
Anirban Kar <anirban.k59@gmail.com>
2023-11-15 19:15:17 +0530

    improve code for Nov 13 challenge

--- tasks.py
+++ tasks.py
@@ -10,17 +10,22 @@ def get_duration(task):
     return task["duration"]
 get_duration({"name": "Task 1", "duration": 12})
 
-def doTasks(tasks, time_to_work):
+def do_tasks(tasks, time_to_work):
     sorted_tasks = sorted(tasks, key=get_duration)
-    tasks_completed = []
+    tasks_completed_names = []
     for task in sorted_tasks:
         if task["duration"] <= time_to_work:
-            tasks_completed.append(task["name"])
+            tasks_completed_names.append(task["name"])
             time_to_work -= task["duration"]
-    return sorted(tasks_completed)
 
-print(doTasks(tasks,6))
+    results = []
+    for task in tasks:
+        if task["name"] in tasks_completed_names:
+            results.append(task["name"])
+    return results
 
+assert do_tasks(tasks, 6) == ["Task 2", "Task 5", "Task 6"]
+assert do_tasks(tasks, 0) == []
 
 
 
