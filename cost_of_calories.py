da938ce57b90e5d1e90710d3d8f5144bfbf444ce
Anirban Kar <anirban.k59@gmail.com>
2023-11-29 20:29:31 +0530

    add challenge for Nov 27; pending cleanup

--- cost_of_calories.py
+++ cost_of_calories.py
@@ -2,15 +2,17 @@ calories = [200, 400, 600, 800]
 prices = [50, 60, 80, 100]
 daily_goal = 1200
 
+from itertools import combinations
+
 def zip_items(calories,prices):
     output = []
     for i in range(len(calories)):
         c = calories[i]
         p = prices[i]
-        output.append({"calories":c,"prices":p})
+        output.append({"calories":c,"price":p})
     return output
 
-from itertools import combinations
+# y = zip_items(calories,prices)
 
 def combos(items):
     all_combos = []
@@ -18,23 +20,29 @@ def combos(items):
         all_combos += combinations(items,i)
     return all_combos
 
+# z = combos(y)
+# for a_combo in z:
+#     print(a_combo)
+
 def sum_of_calories(combo):
     result = 0
     for item in combo:
         result += item["calories"]
     return result
 
-z = combos(zip_items(calories,prices))
-for combo in z:
-    print(sum_of_calories(combo))
+# for a_combo in z:
+#     print(sum_of_calories(a_combo))
 
 def min_cost_for_calories(calories, prices, daily_goal):
-    z = combos(zip_items(calories,prices))
-    valid_combos = filter(lambda combo: sum_of_calories(combo) >= daily_goal,z)
-    prices = []
-    for combo in valid_combos:
-        for item in combo:
-            result.append(item["prices"])
-    return min([prices])
+    cost = []
+    food_items = zip_items(calories,prices)
+    food_items_combos = combos(food_items)
+    valid_combos = filter(lambda combo: sum_of_calories(combo) >= daily_goal,food_items_combos)
+    for each_combo in valid_combos:
+        cost_per_combo = 0
+        for food_item in each_combo:
+            cost_per_combo += (food_item["price"])
+        cost.append(cost_per_combo)
+    return min(cost)
 
-min_cost_for_calories(calories,prices,1200)
+print(min_cost_for_calories(calories,prices,1200))
