ad1386bfd3c30993af466ff34703056a7af9a9cf
Anirban Kar <anirban.k59@gmail.com>
2023-12-07 19:25:33 +0530

    add link, additional test case

--- rotations.py
+++ rotations.py
@@ -1,3 +1,6 @@
+# https://twitter.com/cassidoo/status/1731574854019666130
+# https://buttondown.email/cassidoo/archive/the-best-preparation-for-tomorrow-is-doing-your-9764/
+
 import math
 
 def rotated_num(numbers):
@@ -16,3 +19,4 @@ def rotated_num(numbers):
 assert rotated_num([4, 0, 1, 2, 3]) == 1
 assert rotated_num([7, 9, 10]) == 0
 assert rotated_num([7, 7, 314, 1337, 7]) == 4
+assert rotated_num([5, 4, 3, 2, 1, 0, -1, 6]) == 6
