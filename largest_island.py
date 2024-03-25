5856ebc8a484e396573441d0e11d5dffb1d80712
Anirban Kar <anirban.k59@gmail.com>
2024-03-25 13:00:00 +0530

    refactor challenge for Mar 25, 2024

--- largest_island.py
+++ largest_island.py
@@ -24,35 +24,3 @@ def calculate_island_area(data, i, j, rows, cols):
     if j < cols - 1 and data[i][j + 1] == 1:
         area += calculate_island_area(data, i, j + 1, rows, cols)
     return area
-
-map1 = [
-    [0,1,1,1,0,0,0,1,1],
-    [0,1,1,1,0,1,0,0,0],
-    [0,1,0,0,0,0,0,1,0],
-    [0,0,1,1,0,1,1,1,0],
-    ]
-
-map2 = [
-        [0, 1, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ]
-
-map3 = [
-        [1, 0, 0],
-        [1, 1, 0],
-        [0, 0, 1]
-    ]
-
-map4 = [
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1]
-    ]
-
-assert largest_island(map1) == 7
-assert largest_island(map2) == 1
-assert largest_island(map3) == 3
-assert largest_island(map4) == 25
