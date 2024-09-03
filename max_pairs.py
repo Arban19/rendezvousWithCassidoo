3171ff1e288be4fa4baadcb9c1cbab7c979d7ab0
Arban19 <anirban.k59@gmail.com>
2024-09-03 16:31:32 +0530

    add challenge for 3 September 2024

--- max_pairs.py
+++ max_pairs.py
@@ -1,6 +1,6 @@
 def max_pairs(pairs):
-    left_collection = {}
-    right_collection = {}
+    left_collection, right_collection = {}, {}
+    result = 0
 
     for pair in pairs:
         direction, size = pair.split("-")
@@ -16,8 +16,7 @@ def max_pairs(pairs):
                 right_collection[size] += 1
             else:
                 right_collection[size] = 1    
-                
-    result = 0
+
     for size in left_collection:
         if size in right_collection:
             result += min(left_collection[size], right_collection[size])
@@ -26,4 +25,3 @@ def max_pairs(pairs):
 assert max_pairs(["L-10","R-10","L-11","R-10","L-10","R-11"]) == 3
 assert max_pairs(["L-10","L-11","L-12","L-13"]) == 0
 assert max_pairs(["L-8","L-8","L-8","R-8"]) == 1
-assert max_pairs(["L-8","R-8","R-8","R-8"]) == 1
